c3d802d6aea46fba381ed1e7dc2ae081b86f0d77
Karl Bittner <karl.bittner@outlook.com>
2019-05-05 21:38:23 +0200

    FrequencyTable and EntropyCalculator (untested)

---
+++ FrequencyTable.cpp
@@ -0,0 +1,46 @@
+//
+// Created by karl on 05.05.19.
+//
+
+#include <numeric>
+#include "FrequencyTable.h"
+#include "EntropyCalculator.h"
+#include<map>
+
+
+FrequencyTable::FrequencyTable(const std::vector<std::vector<std::string>> &data, int column,
+                               const std::string& positiveResultName) {
+
+    for (const auto& line : data) {
+        std::string attributeVal = line[column];
+        std::string resultVal = line.back();
+
+        bool outcome = false;
+        if (resultVal == positiveResultName) {
+            outcome = true;
+        }
+
+        attributeFrequencies[attributeVal][outcome] += 1;
+    }
+}
+
+double FrequencyTable::getGain() {
+    EntropyCalculator ec = EntropyCalculator();
+
+    double gain = 0;
+
+    for (const auto& attribute : attributeFrequencies) {
+        std::string attributeName = attribute.first;
+        std::map<bool, int> countAtOutcome = attribute.second;
+
+        std::vector line = std::vector({countAtOutcome[true], countAtOutcome[false]});
+
+        // Total number of occurrences of this attribute in data
+        int sum = std::accumulate(line.begin(), line.end(), 0);
+
+        // Here, the actual formula is applied
+        gain -= (double(sum) / attributeCount) * ec.getEntroy(line, sum);
+    }
+
+    return gain;
+}
---
+++ FrequencyTable.h
@@ -0,0 +1,28 @@
+//
+// Created by karl on 05.05.19.
+//
+
+#ifndef DECISIONTREE_FREQUENCYTABLE_H
+#define DECISIONTREE_FREQUENCYTABLE_H
+
+
+#include <string>
+#include <vector>
+#include <map>
+
+class FrequencyTable {
+public:
+    FrequencyTable(const std::vector<std::vector<std::string>> &data, int column,
+                   const std::string& positiveResultName);
+
+    double getGain();
+
+private:
+    // Maps attribute values to the frequencies of a positive/negative outcome
+    std::map<std::string, std::map<bool, int>> attributeFrequencies;
+
+    int attributeCount;
+};
+
+
+#endif //DECISIONTREE_FREQUENCYTABLE_H
